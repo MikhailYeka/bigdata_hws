bcd3d07fec753e750f6b199bbef4a696b1189ce4
MikhailYeka <mihael.gashkov@gmail.com>
2021-10-25 16:39:29 +0500

    Almost done

---
+++ hw3/generate_data.py
@@ -0,0 +1,26 @@
+import random 
+
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+
+
+NUM_POINTS = 100_000
+
+
+df = pd.DataFrame({'x_0': np.linspace(0, 10, NUM_POINTS)})
+
+for i in range(1, 10):
+	start_point = random.randint(0, 1000)
+	X = np.linspace(start_point, start_point+100, num=NUM_POINTS)
+	df = df.merge(pd.DataFrame({f'x_{i}': X}), how='right', left_index=True, right_index=True)
+
+coefs = np.array([0.1, -23., 132., -109., 28., 10.4, 983., -47., 32., 653.])
+
+df['y'] = df.to_numpy() @ coefs.T + random.randrange(-1, 1)
+
+train_index, test_index = train_test_split(df.index, test_size=0.1)
+
+df.loc[train_index].to_csv('train.csv', index=False)
+df.loc[test_index].to_csv('test.csv', index=False)
+
--- hw3/made_bigdata/src/main/scala/linreg/Main.scala
+++ hw3/made_bigdata/src/main/scala/linreg/Main.scala
@@ -1,13 +1,51 @@
 package linreg
 
 import breeze.linalg._
+import breeze.numerics._
 import java.io._
 
 object Main {
 
   def main(args: Array[String]): Unit = {
 
-    val vec  = DenseVector(1.0, 2.0)
-    println(min(vec))
+    val fileNameTrain = "../train.csv"
+    val fileNameTest = "../test.csv"
+
+    val vec_train  = csvread(new File(fileNameTrain), skipLines = 1)
+    val vec_test  = csvread(new File(fileNameTest), skipLines = 1)
+
+    val x_train = vec_train(::, 0 to 9)
+    val y_train = vec_train(::, 10)
+
+    val x_test = vec_test(::, 0 to 9)
+    val y_test = vec_test(::, 10)
+// Формулу взял отсюда http://www.machinelearning.ru/wiki/index.php?title=Линейная_регрессия_%28пример%29
+    val ones_vector = DenseMatrix.ones[Double](rows = x_train.rows, cols = 1)
+    val A = DenseMatrix.horzcat(ones_vector, x_train)
+    val ws0 = inv(A.t * A)
+    val ws1 = ws0 * A.t
+    val w = ws1 * y_train
+    println(w)
+
+//    Теперь оценим, как эта формула работает на тестовых данных, и какая получилась SSE ошибка
+    val ones_vector1 = DenseMatrix.ones[Double](rows = x_test.rows, cols = 1)
+    val A1 = DenseMatrix.horzcat(ones_vector1, x_test)
+
+    val y_predicted = DenseVector.zeros[Double](A1.rows)
+    var sse: Double = 0
+
+    for (i <- 0 to x_test.rows-1){
+      val y_pred: Double = A1(i, ::) * w
+      sse = sse + (y_pred - y_test(i)) * (y_pred - y_test(i))
+      y_predicted(i) += y_pred
+    }
+//    Оч коряво, но не смог найти, как сделать в бризе красивее
+
+    println(sse)
+
+//    Сохраним результат
+    csvwrite(new File("../predicted.csv"), y_predicted.asDenseMatrix)
+    csvwrite(new File("../w.csv"), w.asDenseMatrix)
+    csvwrite(new File("../diff_true_predicted.csv"), (y_predicted - y_test).asDenseMatrix)
   }
 }
